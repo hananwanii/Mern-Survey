13a8cb974fd00d2e9300188884fcfef3da5fa004
hananwanii <hanan.bunk78@gmail.com>
2023-12-27 02:43:05 +0530

    New Commit2

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-const BASE_URL = process.env.BASE_URL
+const BASE_URL = process.env.BASE_URL;
+
 
 app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://127.0.0.1/surveydb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
+const DB = process.env.DATABASE
 
- db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
-
- db.once('open', function(){
-    console.log('Connected to Database:: MongoDB');
- });
+mongoose.connect(DB,{
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+}).then(()=> console.log("DataBase Connected")).catch((err)=>{
+    console.log(err);
+})
 
 const surveySchema = new mongoose.Schema({
   name: String,
